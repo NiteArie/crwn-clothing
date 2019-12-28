3e55dfde2e0999b6e3d4e982ccbc68946c4bdbf2
Nite <solitude.loner7@gmail.com>
2019-12-28 19:33:09 +0700

    Feature: SignIn/SignUp done

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import './sign-in.styless.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
 
 class Signin extends React.Component {
     constructor(props) {
@@ -16,13 +16,21 @@ class Signin extends React.Component {
         }
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
 
-        this.setState({
-            email: '',
-            password: '',
-        })
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({
+                email: '',
+                password: '',
+            });
+        } catch (error) {
+            console.error(error);
+        }
+        
     }
 
     handleChange = (event) => {
