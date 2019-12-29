2c6c5155af2e674e6b65a2bf9241ce8bb622dfe0
Nite <solitude.loner7@gmail.com>
2019-12-29 14:12:35 +0700

    Feature: CartDropDown upadte

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
+
+import CustomButton from '../custom-button/custom-button.component';
+
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ name, price, imageUrl }) => {
+const CollectionItem = ({item, addItem}) => {
+    const { name, imageUrl, price} = item;
     return (
         <div className="collection-item">
             <div className="image" style={{ 
@@ -13,8 +19,15 @@ const CollectionItem = ({ name, price, imageUrl }) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
+            <CustomButton className="custom-button" inverted
+            onClick={() => addItem(item)}
+            >ADD TO CART</CustomButton>
         </div>
     );
 }
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
