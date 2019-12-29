5cd10ffc13170b2fc77201d0042b4a88d3f8e213
Nite <solitude.loner7@gmail.com>
2019-12-29 12:49:05 +0700

    Feature: Redirect/ Alter Redux Action

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -49,15 +49,20 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" render={props => <HomePage {...props} />} />
           <Route path="/shop" render={props => <ShopPage {...props} />} />
-          <Route path="/signin" render={props => <SignInAndSignUpPage {...props} />} />
+          <Route exact path="/signin" 
+            render={props => this.props.currentUser ? (<Redirect to="/" />) : <SignInAndSignUpPage {...props} />} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
