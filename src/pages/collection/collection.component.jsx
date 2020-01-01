4baa5d011c5f82a77357489ab71d423e90e5e1a5
Nite <solitude.loner7@gmail.com>
2020-01-01 20:28:38 +0700

    Factor: CollectionPage component -> styled-component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,19 +7,25 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
+import {
+    CollectionPageContainer,
+    CollectionPageTitle,
+    ItemsContainer,
+} from './collection.styles';
+
 const CollectionPage = ({ match, collection}) => {
     const { title, items } = collection;
     return (
-        <div className="collection-page">
-            <h2 className="title">{title}</h2>
-            <div className="items">
+        <CollectionPageContainer>
+            <CollectionPageTitle>{title}</CollectionPageTitle>
+            <ItemsContainer>
                 {
                     items.map(item => (
                         <CollectionItem key={item.id} item={item}/>
                     ))
                 }
-            </div>
-        </div>
+            </ItemsContainer>
+        </CollectionPageContainer>
     );
 }
 
