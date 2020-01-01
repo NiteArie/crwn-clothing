272fea440e89aa106d585462591bd24a3b621d7e
Nite <solitude.loner7@gmail.com>
2020-01-01 19:46:35 +0700

    Factor: MenuItem component -> styled-component - Fixed Directory style

--- src/components/MenuItem/menu-item.component.jsx
+++ src/components/MenuItem/menu-item.component.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
+import {
+    MenuItemContainer,
+    MenuItemBackgroundContainer,
+    ContentContainer,
+    ContentTitle,
+    ContentSubtitle
+} from './menu-item.styles';
+
 const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
     return (
-    <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
-        <div className="background-image" 
-        style={{
-            backgroundImage: `url(${imageUrl})`,
-        }} />
-        <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOPS NOW</span>
-        </div>
-    </div>);
+        <MenuItemContainer size={size} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+            <MenuItemBackgroundContainer className="background-image"  />
+            <ContentContainer>
+                <ContentTitle>{title.toUpperCase()}</ContentTitle>
+                <ContentSubtitle className="subtitle">SHOPS NOW</ContentSubtitle>
+            </ContentContainer>
+        </MenuItemContainer>
+    );
 }
 
 export default withRouter(MenuItem);
