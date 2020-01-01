e22501b41f5c0e05e15a28e22f31bb24a30ce85a
Nite <solitude.loner7@gmail.com>
2020-01-01 18:39:56 +0700

    Feature: CheckoutItem component -> styled-component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -6,23 +6,25 @@ import './checkout-item.styles.scss';
 
 import { connect } from 'react-redux';
 
+import { CheckoutItemContainer, ImageContainer, TextContainer, QuantityContainer, RemoveButtonContainer } from './checkout-item.styles';
+
 const CheckoutItem = ({ cartItem, ...otherProps}) => {
     const { name, imageUrl, quantity, price } = cartItem;
     const { removeItem, decreaseQuantity, increaseQuantity } = otherProps;
     return (
-        <div className="checkout-item">
-            <div className="image-container">
+        <CheckoutItemContainer>
+            <ImageContainer>
                 <img src={imageUrl} alt="item" />
-            </div>
-                <span className="name">{name}</span>
-                <span className="quantity">
+            </ImageContainer>
+                <TextContainer>{name}</TextContainer>
+                <QuantityContainer>
                     <div className="arrow" onClick={() => decreaseQuantity(cartItem)}>&#10094;</div>
                     <span className="value">{quantity}</span>
                     <div className="arrow" onClick={() => increaseQuantity(cartItem)}>&#10095;</div>
-                </span>
-                <span className="price">${price}</span>
-            <div className="remove-button" onClick={() => removeItem(cartItem)}>&#10005;</div>
-        </div>
+                </QuantityContainer>
+                <TextContainer>${price}</TextContainer>
+            <RemoveButtonContainer onClick={() => removeItem(cartItem)}>&#10005;</RemoveButtonContainer>
+        </CheckoutItemContainer>
     );
 }
 
