b708e0f78208e7ce9305be1717f4a94bf6acff70
Nite <solitude.loner7@gmail.com>
2020-01-01 18:17:49 +0700

    CartDropdown component to styled-component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -11,9 +11,11 @@ import './cart-dropdown.styles.scss';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
+import { CartDropdownContainer, CartItemsContainer, EmptyMessageSpan } from './cart-dropdown.styles';
+
 const CartDropdown = ({ cartItems, history, toggleCartHidden }) => (
-    <div className="cart-dropdown">
-        <div className="cart-items">
+    <CartDropdownContainer>
+        <CartItemsContainer>
             {
                 cartItems.length 
                 ?
@@ -21,15 +23,16 @@ const CartDropdown = ({ cartItems, history, toggleCartHidden }) => (
                     <CartItem key={cartItem.id} item={cartItem} />    
                 )
                 :
-                <span className="empty-message">Your cart is empty</span>
+                <EmptyMessageSpan>Your cart is empty</EmptyMessageSpan>
             }
-        </div>
+        </CartItemsContainer>
         <CustomButton onClick={() => 
             {
                 history.push('/checkout');
                 toggleCartHidden();
-            }}>GO TO CHECKOUT</CustomButton>
-    </div>
+            }}
+            >GO TO CHECKOUT</CustomButton>
+    </CartDropdownContainer>
 )
 
 const mapStateToProps = (state) => {
