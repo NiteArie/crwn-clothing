206457ac298286f7243af0695fe6515f26cd9e81
Nite <solitude.loner7@gmail.com>
2020-01-02 13:27:56 +0700

    Factor: ShopPage functional component -> class component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,16 +4,21 @@ import { Route, Switch } from 'react-router-dom';
 import CollectionOverview from '../../components/collection-overview/collection-overview.component';
 import CollectionPage from '../../pages/collection/collection.component';
 
-const ShopPage = ({ match }) => {
-    console.log(match);
-    return (
-        <div className="shop-page">
-            <Switch>
-                <Route exact path={`${match.url}`}  render={props => <CollectionOverview {...props} /> } /> 
-                <Route exact path={`${match.path}/:collectionId`} render={props => <CollectionPage {...props} /> }/>
-            </Switch>
-        </div>
-    );
+class ShopPage extends React.Component {
+
+    
+
+    render() {
+        const { match } = this.props;
+        return (
+            <div className="shop-page">
+                <Switch>
+                    <Route exact path={`${match.url}`}  render={props => <CollectionOverview {...props} /> } /> 
+                    <Route exact path={`${match.path}/:collectionId`} render={props => <CollectionPage {...props} /> }/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 
